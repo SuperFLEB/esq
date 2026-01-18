8e4918ab5e204b0a51adb3cb4254bd0cdada3b3d
FLEB <voterudy@voterudy.org>
2026-01-18 11:13:37 -0500

    Add pip repo creation and upload to Pages. Move to Python 3.11.

---
+++ .github/scripts/write_repo.py
@@ -0,0 +1,75 @@
+import json
+import os
+import subprocess as sp
+import tomllib
+
+project_info_cache = None
+releases_info_cache = None
+
+def get_project_info():
+    global project_info_cache
+    if project_info_cache is not None:
+        return project_info_cache
+    with open("pyproject.toml", "rb") as f:
+        project_info_cache = tomllib.load(f)["project"]
+        return project_info_cache
+
+def get_releases_info():
+    global releases_info_cache
+    if releases_info_cache is not None:
+        return releases_info_cache
+
+    releases_list_json = sp.check_output(["gh", "release", "list", "--json", "tagName"])
+    releases_list = json.loads(releases_list_json)
+
+    releases = []
+
+    for release in releases_list:
+        if not release["tagName"].startswith("v"):
+            continue
+        release_data_json = sp.check_output(["gh", "release", "view", release["tagName"], "--json", "name,assets"])
+        release_data = json.loads(release_data_json)
+        wheels = [asset["url"] for asset in release_data["assets"] if "url" in asset and asset["url"].endswith('.whl')]
+        releases.append({"tag": release["tagName"], "wheels": wheels})
+
+    releases_info_cache = releases
+    return releases_info_cache
+
+def get_release_ul():
+    lis = "\n".join(
+        [f"\t\t<li><a href=\"{r['wheels'][0]}\">{r['tag']}</a></li>" for r in get_releases_info()]
+    )
+    return f"\t<ul>\n{lis}\t\n\t</ul>"
+
+pinfo = get_project_info()
+rinfo = get_releases_info()
+
+detail = f"""<html lang="en">
+<head>
+    <title>{pinfo["name"]}</title>
+</head>
+<body>
+    <h1>{pinfo["name"]}</h1>
+    <p>{pinfo["description"]}</p>
+{get_release_ul()}
+</body>
+</html>"""
+
+index = f"""<html lang="en">
+    <head>
+        <title>{pinfo["name"]} repository</title>
+    </head>
+    <body>
+        <h1>{pinfo["name"]} repository</h1>
+        <p>This URL can be referenced as a <tt>pip</tt> repository to install releases of the {pinfo["name"]} package.</p>
+        <a href="{pinfo["name"]}/">{pinfo["name"]}</a>
+    </body>
+</html>"""
+
+os.mkdir("dist_html")
+os.mkdir(f"dist_html/{pinfo['name']}")
+
+with open("dist_html/index.html", "w") as f:
+    f.write(index)
+with open(f"dist_html/{pinfo['name']}/index.html", "w") as f:
+    f.write(detail)
